f4b57333bf05d7fb92234e3a27ac3f2b1782e52c
wojciech-turek <contact@wojciechturek.com>
2022-10-02 21:13:51 +0200

    Return scores only for current league

--- src/routes/score.js
+++ src/routes/score.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import shooterContract from "../contract/shooter-contract";
 import Game from "../models/game";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
+  const currentLeague = await shooterContract.leagueNumber();
   // get 10 games from the database with the highest score and return them
-  const topScores = await Game.find({ ended: true, score: { $gt: 0 } })
+  const topScores = await Game.find({
+    ended: true,
+    currentLeague: currentLeague,
+    score: { $gt: 0 },
+  })
     .sort({ score: -1 })
     .limit(10)
     .select(["score", "creatorAddress"]);
